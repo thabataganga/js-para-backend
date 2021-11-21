1fd236c88c4a7550c04010d145afe9dfb02b6df9
thabataganga <ganga.thabata@gmail.com>
2021-11-21 17:33:24 -0300

    exercicio

--- Declarando_Variaveis/conversoes.js
+++ Declarando_Variaveis/conversoes.js
@@ -2,6 +2,8 @@
 // booleanos
 
 // conversao implicita
+// conversao explicita
+
 
 const numero = 456;
 const numeroString = "456";
@@ -21,4 +23,27 @@ console.log(numero === numeroString);
 // String()
 console.log("A+B = " + (numero + Number(numeroString)));
 
-// conversao explicita
+
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+let telefone2 = 12341234;
+console.log("O telefone é " + telefone2.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+
+let usuarioConectado = false;
+console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+usuarioConectado = true;
+console.log(String(usuarioConectado)); // agora teremos uma string “true”.
+
+// Vamos calcular a área de um retângulo
+let largura = "10";
+let altura = "5";
+console.log(Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da da multiplicação
+
+// Podemos usar o operador de soma + para fazer a conversão de textos para números, colocando-os antes das variáveis
+console.log(+largura * +altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+let usuarioConectado2 = false;
+console.log(Number(usuarioConectado2)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado2 = true;
+console.log(Number(usuarioConectado2)); // agora teremos a conversão de true (verdadeiro) para o número 1.
