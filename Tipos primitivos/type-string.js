7081e56190d235e84db6699186abe8cef74191a2
thabataganga <ganga.thabata@gmail.com>
2021-11-21 08:28:42 -0300

    trabalhando com string

---
+++ Tipos primitivos/type-string.js
@@ -0,0 +1,23 @@
+const text1 = "Olá, mundo!";
+const text2 = 'Olá, mundo!';
+const senha = "H787oiad9!*Maefl";
+const StringDeNumeros = "34567";
+
+
+// Variando aspas
+
+const citacao1 = 'O Leo disse "oi"';
+console.log(citacao1);
+
+const citacao2 = "O Leo disse 'oi'";
+console.log(citacao2);
+
+// concatenação (+)
+
+const citacao = "Meu nome é ";
+const meuNome = "Thabata";
+
+console.log(citacao + meuNome);
+
+
+// Template string ou template literal
