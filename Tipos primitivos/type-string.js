18c7723d048a7446e9e9a3336c6c58d4213d7ede
thabataganga <ganga.thabata@gmail.com>
2021-11-21 08:56:34 -0300

    trabalhando com numeros 2

--- Tipos primitivos/type-string.js
+++ Tipos primitivos/type-string.js
@@ -1,23 +1,51 @@
 const text1 = "Olá, mundo!";
 const text2 = 'Olá, mundo!';
-const senha = "H787oiad9!*Maefl";
+const Senha = "H787oiad9!*Maefl";
 const StringDeNumeros = "34567";
 
 
-// Variando aspas
+/// Variando aspas
 
 const citacao1 = 'O Leo disse "oi"';
-console.log(citacao1);
+console.log("Aspas 1: " + citacao1);
 
 const citacao2 = "O Leo disse 'oi'";
-console.log(citacao2);
+console.log("Aspas 2: " + citacao2);
 
-// concatenação (+)
+/// concatenação (+)
 
 const citacao = "Meu nome é ";
 const meuNome = "Thabata";
 
-console.log(citacao + meuNome);
+console.log("Concatenação: " + citacao + meuNome);
 
+/// Caracteres especiais com unicode
 
-// Template string ou template literal
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+console.log("Unicodes: ")
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+/// Case sensitive
+// o JavaScript diferencia minúsculas e maiúsculas, tanto nos valores dos dados quanto no código que escrevemos.
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+// convertendo todos os caracteres para minusculos
+const inputMinusculo = input.toLowerCase();
+
+console.log("Booleano: " + (cidade === inputMinusculo));
+
+/// O Tamanho de uma String 
+const senha = "minhaSenha123"
+console.log("Tamanho: " + senha.length) // 13 caracteres
+
+// length não leva () no final, pois é uma propriedade e não uma função/metodo
+
+/// Template string ou template literal
