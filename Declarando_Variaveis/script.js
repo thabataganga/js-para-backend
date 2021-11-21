0a05d34c26a6e76f6e54aebecc0c8aa86aa03f56
thabataganga <ganga.thabata@gmail.com>
2021-11-21 14:09:47 -0300

    var, let e const

---
+++ Declarando_Variaveis/script.js
@@ -0,0 +1,38 @@
+// var
+
+/* var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area); */
+
+// let
+
+/* let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+
+if (forma === 'retangulo') {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area); */
+
+// const
+
+// Esse tipo de varialvel não pode ser alterada dentro do programa, é uma constante -> serve para proteger variaveis que não podem ser alteradas
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
