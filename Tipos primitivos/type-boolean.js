80712b8d74cfb05e92fbdf42652f8ce631759b21
thabataganga <ganga.thabata@gmail.com>
2021-11-21 09:06:57 -0300

    trabalhando com boolean

---
+++ Tipos primitivos/type-boolean.js
@@ -0,0 +1,16 @@
+/// Boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero + " é igual " + segundoNumero + "?");
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Thabata";
+const texto2 = "thabata";
+
+console.log(texto1 + " é igual " + texto2 + "?");
+console.log(texto1 === texto2);
