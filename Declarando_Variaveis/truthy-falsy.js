cf4fd8325474cb100bd8499982f8363b39b9c080
thabataganga <ganga.thabata@gmail.com>
2021-11-21 16:42:04 -0300

    trusty e falsy

---
+++ Declarando_Variaveis/truthy-falsy.js
@@ -0,0 +1,26 @@
+// booelan
+let usuarioLogado = true;
+let contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+/* console.log(0 == false);
+console.log("" == false);
+console.log(1 == true); */
+
+// undefined
+
+// null ===> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+// verificar o tipo do dado
+console.log(typeof numero);
+console.log(typeof texto);
