e9545cb78597b600f6ecf134d8c030ea57f28d52
thabataganga <ganga.thabata@gmail.com>
2021-11-21 18:05:28 -0300

    jd e node

---
+++ JS_e_NodeJS/script.js
@@ -0,0 +1,14 @@
+// tipagem dinamica
+// untyped
+// No JS não precisa declarar o tipo de variavel
+
+let minhaVar = 567;
+console.log(minhaVar);
+
+minhaVar = "texto";
+console.log(minhaVar);
+
+minhaVar = true;
+console.log(minhaVar);
+
+// O node é um interpretador e nao uma linguagem
