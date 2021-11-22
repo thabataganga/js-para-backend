a237417a1ab5809353efedbeffca9f4664a729e6
thabataganga <ganga.thabata@gmail.com>
2021-11-22 03:18:24 -0300

    organizacao das pastas

---
+++ 4_Operadores/comparacoes.js
@@ -0,0 +1,12 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+
+// typeof
+console.log(typeof numero);
+console.log(typeof texto);
