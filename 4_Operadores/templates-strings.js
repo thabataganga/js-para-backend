d528c2d76f3dace639c56947b0d4e714b3ad16f0
thabataganga <ganga.thabata@gmail.com>
2021-11-22 04:16:49 -0300

    template string

---
+++ 4_Operadores/templates-strings.js
@@ -0,0 +1,9 @@
+let nome = "Thabata";
+let idade = 28;
+let cidadeDeNascimento = "São Paulo"
+
+/* const apresentacao = "Meu nome é " + nome + ", eu tenho " + idade + " anos e nasci em " + cidadeDeNascimento; */
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
