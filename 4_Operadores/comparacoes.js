ab6fe7bc57d87031cfd8b33e65e691dffe20d571
thabataganga <ganga.thabata@gmail.com>
2021-11-22 03:23:56 -0300

    operadores

--- 4_Operadores/comparacoes.js
+++ 4_Operadores/comparacoes.js
@@ -10,3 +10,34 @@ console.log(numero === texto);
 // typeof
 console.log(typeof numero);
 console.log(typeof texto);
+
+/* == só compara o valor
+=== compara o valor e o tipo de dado */
+
+// As boas práticas pedem para usar sempre === e realizar a conversão dos tipos
+
+
+// Conversão Explicita ===
+
+/* Number();
+String(); */
+
+console.log(numero === Number(texto));
+console.log(texto === String(numero));
+
+/* Até agora praticamos com alguns dos operadores do JavaScript, especialmente alguns dos utilizados em operações matemáticas:
+
+nome	operador
+Atribuição de adição	x + y
+Atribuição de subtração	x - y
+Atribuição de multiplicação	x * y
+Atribuição de divisão	x / y
+
+Começamos com os operadores mais utilizados, pois sempre que aprendemos uma linguagem de programação nova, o melhor é absorvermos mais conhecimentos aos poucos sobre cada assunto, em vez de tentar dominar tudo de uma vez.
+
+Porém, a lista de operadores é extensa e há muitas formas de utilizá-los. Além das operações matemáticas básicas, do = para atribuição de valor em uma variável e das comparações com == e ===, utilizamos operadores para diversas outras tarefas de código, por exemplo:
+
+||: Operador “ou”, retorna true caso uma condição seja válida;
+&&: Operador “e”, retorna true somente se todas as condições forem válidas;
+!= e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
+A documentação do MDN sobre operadores tem a lista completa, com vários exemplos. Invista um tempo para se familiarizar com cada um deles e tente pensar em exemplos de uso para cada um. */
