68c26860afe5400361b6187bfaba30649bce6c6e
thabataganga <ganga.thabata@gmail.com>
2021-11-30 13:24:22 -0300

    atualiar array exercicio

--- 2_Arrays/2_Alterando_Arrays/atualizando.js
+++ 2_Arrays/2_Alterando_Arrays/atualizando.js
@@ -6,3 +6,11 @@ console.log(`Lista antiga: ${listaDeChamada}`);
 listaDeChamada.splice(2, 0, "Rodrigo");
 
 console.log(`Lista de chamada: ${listaDeChamada}`);
+
+// Exercicio
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1, 0, '🐠')
+animaisDoAquario.splice(3, 2, '🐟')
+
+console.log(animaisDoAquario)
