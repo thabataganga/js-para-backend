010cc42246d35cc4ef288a706149a13dc09cd1d1
thabataganga <ganga.thabata@gmail.com>
2021-12-01 21:29:48 -0300

    matrizes

---
+++ 2_Arrays/2_Alterando_Arrays/lista-de-duas-dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const mediaDosAlunos = [10, 7, 9, 6];
+
+let listaDeNotasEAlunos = [alunos, mediaDosAlunos];
+
+//console.log(listaDeNotasEAlunos[0][0]);
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}`);
+console.log(`${listaDeNotasEAlunos[0][1]}, sua média é ${listaDeNotasEAlunos[1][1]}`);
+console.log(`${listaDeNotasEAlunos[0][2]}, sua média é ${listaDeNotasEAlunos[1][2]}`);
+console.log(`${listaDeNotasEAlunos[0][3]}, sua média é ${listaDeNotasEAlunos[1][3]}`);
