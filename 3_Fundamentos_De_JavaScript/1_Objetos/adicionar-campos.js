f5173b901bc0b94028a8c6572afe1a39721e9fd9
thabataganga <ganga.thabata@gmail.com>
2021-12-02 01:50:04 -0300

    adicionando campos em objetos

---
+++ 3_Fundamentos_De_JavaScript/1_Objetos/adicionar-campos.js
@@ -0,0 +1,12 @@
+let cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "12365242625",
+    email: "andre@email.com"
+};
+
+console.log(cliente);
+
+cliente.fone = "213123123123123";
+
+console.log(cliente);
