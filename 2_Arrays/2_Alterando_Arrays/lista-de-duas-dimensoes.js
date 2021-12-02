7ccc83c7a15041fbeb774d2105fcd417042f88d6
thabataganga <ganga.thabata@gmail.com>
2021-12-01 21:31:59 -0300

    matrizes n dimensoes

--- 2_Arrays/2_Alterando_Arrays/lista-de-duas-dimensoes.js
+++ 2_Arrays/2_Alterando_Arrays/lista-de-duas-dimensoes.js
@@ -9,3 +9,11 @@ console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1
 console.log(`${listaDeNotasEAlunos[0][1]}, sua média é ${listaDeNotasEAlunos[1][1]}`);
 console.log(`${listaDeNotasEAlunos[0][2]}, sua média é ${listaDeNotasEAlunos[1][2]}`);
 console.log(`${listaDeNotasEAlunos[0][3]}, sua média é ${listaDeNotasEAlunos[1][3]}`);
+
+let idades = [30, 35, 28]
+let nomes = ["Ana", "Juliana", "Leonardo"]
+let faculdade = [false, true, true]
+
+let funcionarios = [nomes, idades, faculdade]
+
+console.log(funcionarios);
