939afd9702165fbbc1d1de05213b0ca3cddb957b
thabataganga <ganga.thabata@gmail.com>
2021-12-02 01:05:29 -0300

    intro objetos

---
+++ 3_Fundamentos_De_JavaScript/1_Objetos/objetos.js
@@ -0,0 +1,13 @@
+const listaCPF = ["12365242625", "9242624265", "9352524652"];
+
+// array
+const cliente = ["nome", "André", "idade", 36];
+
+// objeto
+/// Quando precisa fazer uma abstração do mundo real
+let cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "12365242625",
+    email: "andre@email.com"
+};
